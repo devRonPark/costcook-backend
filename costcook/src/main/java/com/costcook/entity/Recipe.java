2c2ba1df5e8733752ff75d18e43c8de79ab06daa
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-16 13:47:22 +0900

    Feat: 사용자 관련 엔티티 구현

--- costcook/src/main/java/com/costcook/entity/Recipe.java
+++ costcook/src/main/java/com/costcook/entity/Recipe.java
@@ -12,8 +12,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- costcook/src/main/java/com/costcook/service/impl/RecipeServiceImpl.java
+++ costcook/src/main/java/com/costcook/service/impl/RecipeServiceImpl.java
@@ -1,8 +1,12 @@
 package com.costcook.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.costcook.domain.response.RecipeResponse;
 import com.costcook.repository.RecipeRepository;
+import com.costcook.service.RecipeService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class RecipeServiceImpl {
+public class RecipeServiceImpl implements RecipeService {
 	
 	private final RecipeRepository recipeRepository;
+
+	@Override
+	public List<RecipeResponse> getAllRecipe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RecipeResponse getRecipeById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
