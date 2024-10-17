67ef6f5c6e0f589e1645f307474cf840be48df47
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-17 15:28:07 +0900

    Feat: 인증 코드 발송 이메일 본문 수정

--- costcook/src/main/java/com/costcook/service/impl/EmailServiceImpl.java
+++ costcook/src/main/java/com/costcook/service/impl/EmailServiceImpl.java
@@ -14,10 +14,20 @@ public class EmailServiceImpl implements EmailService {
 	@Override
 	public void sendVerificationCode(String email, String verificationCode) {
 		SimpleMailMessage message = new SimpleMailMessage();
-		message.setTo(email);
-        message.setSubject("인증 코드");
-        message.setText("인증 코드는: " + verificationCode);
-        mailSender.send(message);
+	    message.setTo(email);
+	    message.setSubject("CostCook 인증 코드 안내");
+
+	    String emailBody = "안녕하세요!\n\n" +
+	            "CostCook에 가입해 주셔서 감사합니다! 🎉\n\n" +
+	            "1주일 예산에 맞춤형 레시피를 추천해 드리는 서비스를 이용하시려면 아래의 인증 코드를 입력해 주세요:\n\n" +
+	            "🔑 **인증 코드:** " + verificationCode + "\n\n" +
+	            "이 인증 코드는 서비스 가입을 완료하는 데 필요하며, 한 번만 사용 가능합니다.\n" +
+	            "코드를 입력한 후, 레시피 추천 서비스를 즐기세요! 🍽️\n\n" +
+	            "감사합니다!\n\n" +
+	            "CostCook 팀 드림";
+
+	    message.setText(emailBody);
+	    mailSender.send(message);
 	}
 
 }
