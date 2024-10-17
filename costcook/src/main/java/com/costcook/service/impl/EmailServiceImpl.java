4ac326933d843f156fee41690766d11f6cf6e5fc
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-17 16:45:24 +0900

    Feat: 인증 코드 확인 비즈니스 로직 구현

--- costcook/src/main/java/com/costcook/service/impl/EmailServiceImpl.java
+++ costcook/src/main/java/com/costcook/service/impl/EmailServiceImpl.java
@@ -7,9 +7,14 @@ import com.costcook.entity.EmailVerification;
 import com.costcook.repository.EmailVerificationRepository;
 import com.costcook.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDateTime;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
@@ -37,5 +42,21 @@ public class EmailServiceImpl implements EmailService {
 
 		emailVerificationRepository.save(emailVerification);
 	}
+	
+	public boolean verifyCode(String email, String verificationCode) {
+		log.info("verifyCode 메소드 실행 시작");
+        EmailVerification verification = emailVerificationRepository.findTopByUserEmailOrderByCreatedAtDesc(email)
+                .orElseThrow(() -> new IllegalArgumentException("이메일을 찾을 수 없습니다."));
+        log.info("verification 정보: {}", verification.toString());
+        
+        // 인증 코드 만료 확인
+        if (verification.getExpiresAt().isBefore(LocalDateTime.now())) {
+            throw new IllegalStateException("인증 코드가 만료되었습니다.");
+        }
+
+        // 인증 코드 일치 여부 확인
+        log.info("인증 코드 일치여부 확인: {}", verification.getVerificationCode().equals(verificationCode));
+        return verification.getVerificationCode().equals(verificationCode);
+    }
 
 }
