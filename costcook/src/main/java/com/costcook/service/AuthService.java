9d3aab910caed722ad6e339237233c31e3fcead2
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-21 10:06:29 +0900

    Feat: AuthServiceImpl 구현: 회원 가입 및 로그인 기능 추가, 유효성 검증 로직 및 소셜 계정 저장 기능 포함

--- costcook/src/main/java/com/costcook/service/AuthService.java
+++ costcook/src/main/java/com/costcook/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.costcook.service;
 
 import com.costcook.domain.request.SignUpOrLoginRequest;
-import com.costcook.domain.request.SignUpOrLoginResponse;
+import com.costcook.domain.response.SignUpOrLoginResponse;
 
 public interface AuthService {
 	public SignUpOrLoginResponse signUpOrLogin(SignUpOrLoginRequest request);
