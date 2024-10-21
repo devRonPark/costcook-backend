e6c50950d566433d9de6dcef06efdd7bc6ddbdfa
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-21 10:30:38 +0900

    Fix: AuthServiceImpl 필요한 주석 추가 및 SignUpOrLogin 응답 DTO 생성자 Lombok 어노테이션으로 변경

--- costcook/src/main/java/com/costcook/domain/response/SignUpOrLoginResponse.java
+++ costcook/src/main/java/com/costcook/domain/response/SignUpOrLoginResponse.java
@@ -1,22 +1,18 @@
 package com.costcook.domain.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class SignUpOrLoginResponse {
     private String message;
     private String accessToken;
-    private String refreshToken;
     private boolean isNewUser;
 
     public SignUpOrLoginResponse(String message) {
         this.message = message;
-    }
-
-    public SignUpOrLoginResponse(String message, String accessToken, String refreshToken, boolean isNewUser) {
-        this.message = message;
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-        this.isNewUser = isNewUser;
     }
 }
--- costcook/src/main/java/com/costcook/service/impl/AuthServiceImpl.java
+++ costcook/src/main/java/com/costcook/service/impl/AuthServiceImpl.java
@@ -25,79 +25,82 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
-	private final UserRepository userRepository;
-	private final SocialAccountRepository socialAccountRepository;
-	private final TokenUtils tokenUtils;
-	
-	@Override
-	public SignUpOrLoginResponse signUpOrLogin(SignUpOrLoginRequest request, HttpServletResponse response) {
-		// 1. Request DTO 유효성 검증
-		validateSignUpOrLoginRequest(request);
+    private final UserRepository userRepository;
+    private final SocialAccountRepository socialAccountRepository;
+    private final TokenUtils tokenUtils;
 
-		// 2. email 필드를 기준으로 회원 가입 여부 확인
-		// - 회원이 이미 존재하는 경우 로그인 처리
-		// - 신규 회원인 경우 회원가입 처리
-		User user = userRepository.findByEmail(request.getEmail())
+    @Override
+    public SignUpOrLoginResponse signUpOrLogin(SignUpOrLoginRequest request, HttpServletResponse response) {
+        // 1. Request DTO 유효성 검증
+        validateSignUpOrLoginRequest(request);
+
+        // 2. email 필드를 기준으로 회원 가입 여부 확인
+        User user = userRepository.findByEmail(request.getEmail())
                 .orElseGet(() -> registerNewUser(request));
 
-		log.info("새로 가입한 회원 정보: {}", user);
-		// 3. 액세스 토큰, 리프레시 토큰 발급
-		Map<String, String> tokenMap = tokenUtils.generateToken(user);
-		String accessToken = tokenMap.get("accessToken");
-		String refreshToken = tokenMap.get("refreshToken");
-		log.info("새로 발급된 액세스 토큰: {}", accessToken);
-		log.info("새로 발급된 리프레시 토큰: {}", refreshToken);
-		
-		// 4. 발급된 리프레시 토큰 사용자 테이블에 저장.
-		user.setRefreshToken(refreshToken);
-		userRepository.save(user);
-		
-		// 5. 응답
-		// 생성된 리프레시 토큰을 cookie에 담아 응답
-		tokenUtils.setRefreshTokenCookie(response, refreshToken);
-		tokenUtils.setAccessTokenCookie(response, accessToken);
-		
-		// SignUpOrLoginResponse 객체 생성
-	    boolean isNewUser = user.getCreatedAt().isAfter(LocalDateTime.now().minusMinutes(1)); // 1분 이내에 생성된 경우 신규 사용자로 간주
-	    SignUpOrLoginResponse signUpOrLoginResponse = SignUpOrLoginResponse.builder()
-	    		.message(isNewUser ? "회원가입 후 로그인이 완료되었습니다." : "로그인에 성공했습니다.")
-	            .accessToken(accessToken)
-	            .isNewUser(isNewUser)
-	            .build();
-		return signUpOrLoginResponse;
-	}
+        log.info("새로 가입한 회원 정보: {}", user);
 
-	private void validateSignUpOrLoginRequest(SignUpOrLoginRequest request) {
-		log.info(request.toString());
-		if (request.getSocialKey() == null || request.getEmail() == null || request.getProvider() == null) {
-			throw new MissingFieldException();
-		}
+        // 3. 액세스 토큰, 리프레시 토큰 발급
+        Map<String, String> tokenMap = tokenUtils.generateToken(user);
+        String accessToken = tokenMap.get("accessToken");
+        String refreshToken = tokenMap.get("refreshToken");
+        log.info("새로 발급된 액세스 토큰: {}", accessToken);
+        log.info("새로 발급된 리프레시 토큰: {}", refreshToken);
+        
+        // 4. 발급된 리프레시 토큰 사용자 테이블에 저장
+        user.setRefreshToken(refreshToken);
+        userRepository.save(user);
+        
+        // 5. 쿠키에 생성된 리프레시 토큰과 액세스 토큰을 담아 응답
+        tokenUtils.setRefreshTokenCookie(response, refreshToken);
+        tokenUtils.setAccessTokenCookie(response, accessToken);
+        
+        // SignUpOrLoginResponse 객체 생성
+        boolean isNewUser = user.getCreatedAt().isAfter(LocalDateTime.now().minusMinutes(1)); // 1분 이내에 생성된 경우 신규 사용자로 간주
+        SignUpOrLoginResponse signUpOrLoginResponse = SignUpOrLoginResponse.builder()
+                .message(isNewUser ? "회원가입 후 로그인이 완료되었습니다." : "로그인에 성공했습니다.")
+                .accessToken(accessToken)
+                .isNewUser(isNewUser)
+                .build();
+        
+        return signUpOrLoginResponse;
+    }
 
-		if (!isValidProvider(request.getProvider())) {
-			throw new InvalidProviderException("적절하지 않은 소셜 로그인 제공자입니다.");
-		}
-	}
+    // SignUpOrLoginRequest의 필수 필드를 검증
+    private void validateSignUpOrLoginRequest(SignUpOrLoginRequest request) {
+        log.info(request.toString());
+        if (request.getSocialKey() == null || request.getEmail() == null || request.getProvider() == null) {
+            throw new MissingFieldException();
+        }
 
-	private boolean isValidProvider(String provider) {
-		return provider.equals("kakao") || provider.equals("google");
-	}
-	
-	private User registerNewUser(SignUpOrLoginRequest request) {
+        if (!isValidProvider(request.getProvider())) {
+            throw new InvalidProviderException("적절하지 않은 소셜 로그인 제공자입니다.");
+        }
+    }
+
+    // 지원되는 소셜 로그인 제공자인지 확인
+    private boolean isValidProvider(String provider) {
+        return provider.equals("kakao") || provider.equals("google");
+    }
+    
+    // 신규 사용자 등록 및 소셜 계정 정보 저장
+    private User registerNewUser(SignUpOrLoginRequest request) {
         User newUser = new User();
         newUser.setEmail(request.getEmail());
         // 추가적인 사용자 정보 설정...
+        
+        // 신규 사용자 저장
         User savedUser = userRepository.save(newUser);
 
         // 소셜 계정 정보 저장
         SocialAccount socialAccount = SocialAccount
-        	.builder()
-        	.provider(PlatformTypeEnum.fromString(request.getProvider()))
-        	.socialKey(request.getSocialKey())
-        	.user(savedUser)
-        	.build();
+                .builder()
+                .provider(PlatformTypeEnum.fromString(request.getProvider()))
+                .socialKey(request.getSocialKey())
+                .user(savedUser)
+                .build();
         socialAccountRepository.save(socialAccount);
 
         return savedUser;
     }
-
 }
