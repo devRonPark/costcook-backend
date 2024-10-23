9aa1b87d8a626238b7b4848348281eca8f9fe887
bennett <licht131@naver.com>
2024-10-23 15:50:10 +0900

    Feat; 재료, 카테고리, 단위 엔티티 생성

---
+++ costcook/src/main/java/com/costcook/entity/Category.java
@@ -0,0 +1,37 @@
+package com.costcook.entity;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(updatable = false)
+  private Long id;
+
+  @Column(nullable = false, length = 100)
+  private String name;
+
+  @Column(nullable = false, name = "parent_id")
+  @Builder.Default()
+  private Long parentId = 0;
+  
+}
