41b0ff06c29fc03d5023b2def7a157dbda0d19b3
bennett <licht131@naver.com>
2024-10-23 12:40:01 +0900

    Feat: 카테고리, 재료, 단위 Entity 추가

---
+++ costcook/src/main/java/com/costcook/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.costcook.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminController {
+
+
+}
