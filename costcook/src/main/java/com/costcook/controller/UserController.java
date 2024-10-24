c6c52c5a2664e16cc046c1b1b85f1648be7e2a39
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-24 12:29:40 +0900

    Feat: 사용자 기피 재료, 선호 재료 업데이트 로직 구현 및 사용자 업데이트 API 엔드포인트에 적용

--- costcook/src/main/java/com/costcook/controller/UserController.java
+++ costcook/src/main/java/com/costcook/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<?> updateMyInfo(
+    public ResponseEntity<String> updateMyInfo(
         @AuthenticationPrincipal User userDetails, // 사용자 정보 가져오기,
         @ModelAttribute UserUpdateRequest requestDTO
     ) {
@@ -53,17 +53,9 @@ public class UserController {
         log.info("요청 본문 정보: {}", requestDTO.toString());
 
         // 사용자 정보 업데이트 로직 서비스에 위임
-        userService.updateUserInfo(userDetails.getId(), requestDTO);
+        userService.updateUserInfo(userDetails, requestDTO);
 
-        // 4. 선호 재료 및 기피 재료 업데이트
-        // - requestDTO에서 선호 재료(preferences)와 기피 재료(dislikedIngredients)를 추출합니다.
-        // - 이를 사용자의 프로필에 업데이트합니다.
-        // - 기존에 저장된 리스트가 있을 경우, 이를 업데이트하거나 새로 추가합니다.
-
-
-
-        // 5. 성공 응답 반환
-        // - 모든 작업이 완료된 후, 성공적인 응답 메시지를 반환합니다.
+        // 성공 응답 반환
         return ResponseEntity.ok("내 정보가 성공적으로 업데이트되었습니다.");
     }
 }
