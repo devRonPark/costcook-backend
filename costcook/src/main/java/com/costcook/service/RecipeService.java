ebaf77c00bb1c83b5b18b65951f03b27c81989cb
유현준 <140572350+YOOHYUNJUNE@users.noreply.github.com>
2024-10-28 14:42:00 +0900

    fix:레시피목록,상세보기 코드 최적화, 객체내용 수정 :  RecipeItem -> Recipe, RecipeListResponse > long recipeId -> Long recipeId

--- costcook/src/main/java/com/costcook/service/RecipeService.java
+++ costcook/src/main/java/com/costcook/service/RecipeService.java
@@ -12,6 +12,6 @@ public interface RecipeService {
 	// 레시피 총 개수
 	long getTotalRecipes();
 	
-	// 레시피 ID를 통해 레시피 가져오기 [상세보기]
+	// 레시피 ID를 통해 레시피 가져오기 -> 상세보기
 	RecipeResponse getRecipeById(Long id);
 }
