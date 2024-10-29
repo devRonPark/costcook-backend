ac2880fadc0ddfa935bdfbb9e861bdc0e3f4c4c6
bennett <licht131@naver.com>
2024-10-29 16:58:59 +0900

    chore: PathVariable 및 RequestParam 어노테이션 파라미터 설정 누락 해결 및 id 변수명 recipeId 로 변경

--- costcook/src/main/java/com/costcook/controller/AdminController.java
+++ costcook/src/main/java/com/costcook/controller/AdminController.java
@@ -32,7 +32,7 @@ public class AdminController {
   private final AdminService adminService;
 
   @GetMapping("/ingredients")
-  public ResponseEntity<List<IngredientSearchResponse>> getIngredientList(@RequestParam String keyword) {
+  public ResponseEntity<List<IngredientSearchResponse>> getIngredientList(@RequestParam("keyword") String keyword) {
 
     log.info("Keyword received: {}", keyword);
 
@@ -41,11 +41,11 @@ public class AdminController {
     return ResponseEntity.ok(responseList);
   }
 
-  @GetMapping("/recipes/{id}/ingredients")
-  public ResponseEntity<List<RecipeIngredientResponse>> getRecipeIngredients(@PathVariable Long id) {
+  @GetMapping("/recipes/{recipeId}/ingredients")
+  public ResponseEntity<List<RecipeIngredientResponse>> getRecipeIngredients(@PathVariable("recipeId") Long recipeId) {
     try {
       // 서비스에서 이미 변환된 DTO 리스트를 받아옵니다.
-      List<RecipeIngredientResponse> ingredientResponses = adminService.findIngredientsByRecipeId(id);
+      List<RecipeIngredientResponse> ingredientResponses = adminService.findIngredientsByRecipeId(recipeId);
       return ResponseEntity.ok(ingredientResponses);
     } catch (Exception e) {
       log.error("레시피 재료 조회 중 오류 발생: " + e.getMessage(), e);
@@ -79,14 +79,14 @@ public class AdminController {
     }
   }
 
-  @PatchMapping("/recipes/{id}")
+  @PatchMapping("/recipes/{recipeId}")
   public ResponseEntity<String> updateRecipe(
-      @PathVariable Long id,
+      @PathVariable("recipeId") Long recipeId,
       @RequestPart("recipe") AdminRecipeRegisterRequest recipe,  // JSON 데이터를 받음 (Blob으로 감싸진 JSON)
       @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnailFile) {
     try {
       
-      log.info("레시피 수정 요청: 레시피 ID: " + id);
+      log.info("레시피 수정 요청: 레시피 ID: " + recipeId);
       log.info("수정할 레시피 제목: " + recipe.getTitle());
       log.info("레시피 썸네일 삭제 상태 : {}", recipe.isThumbnailDeleted());
 
@@ -97,7 +97,7 @@ public class AdminController {
       }
 
       // 수정 서비스 호출
-      boolean result = adminService.updateRecipe(id, recipe, thumbnailFile);
+      boolean result = adminService.updateRecipe(recipeId, recipe, thumbnailFile);
 
       if (!result) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("레시피 수정에 실패했습니다.");
@@ -112,10 +112,10 @@ public class AdminController {
     }
   }
 
-  @DeleteMapping("/recipes/{id}")
-  public ResponseEntity<String> deleteRecipe(@PathVariable Long id) {
+  @DeleteMapping("/recipes/{recipeId}")
+  public ResponseEntity<String> deleteRecipe(@PathVariable("recipeId") Long recipeId) {
     try {
-      adminService.deleteRecipe(id);
+      adminService.deleteRecipe(recipeId);
       return ResponseEntity.ok("레시피가 성공적으로 삭제되었습니다.");
     } catch (IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("레시피를 잦을 수 없습니다.");
@@ -123,8 +123,4 @@ public class AdminController {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("레시피 삭제 중 오류가 발생했습니다.");
     }
   }
-
-
-  
-
 }
